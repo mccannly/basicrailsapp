30ea56f7584939160094a840ecb0b756de4e72e1
Lisa McCann <mccannly@gmail.com>
2018-01-01 16:33:35 -0800

    3.8 fixed routes typo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
-  root 'simple_pasges#index'
+  root 'simple_pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
