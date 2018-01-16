eaa9aef6960267219648b691f9b53e36360d6d1c
Lisa McCann <mccannly@gmail.com>
2018-01-16 15:32:54 -0800

    4.6 Orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,11 @@
 class OrdersController < ApplicationController
 
   def index
+    @orders = Order.includes(:product).all
   end
 
   def show
+    @orders = Order.includes(:product).all
   end
 
   def new
