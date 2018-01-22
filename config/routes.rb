5c891a66e5061823251cfa8c3de90d771fedd1ee
Lisa McCann <mccannly@gmail.com>
2018-01-22 11:52:21 -0800

    5.1 routes, added relative links to Nav

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
 
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+
+  get 'simple_pages/landing_page'
+  
+  root 'simple_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
